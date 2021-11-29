f9a0f094a4fa3f2fdb270b94e25f07ca1028d887
ichebyki <ichebyki@gmail.com>
2021-11-30 02:51:34 +0700

    Update test_hello.py
    commit #3

--- hello/test_hello.py
+++ hello/test_hello.py
@@ -15,5 +15,9 @@ class TestHello(unittest.TestCase):
         """sample test with unicode"""
         self.assertEqual(hello(u'world'), u'hello world')
 
+    def test_world_ru(self):
+        """sample test with ru text"""
+        self.assertEqual(hello(u'мир'), u'привет мир')
+
 
 
